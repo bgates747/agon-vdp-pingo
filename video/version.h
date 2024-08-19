7050951f7fa441230499b1f433b408407a132e67
bgates747 <brandon.gates@gmail.com>
2024-08-19 11:59:44 -0400

    bump version to alpha 4

--- video/version.h
+++ video/version.h
@@ -4,7 +4,7 @@
 #define		VERSION_MAJOR		2
 #define		VERSION_MINOR		9
 #define		VERSION_PATCH		2
-#define		VERSION_CANDIDATE	3			// Optional
+#define		VERSION_CANDIDATE	4			// Optional
 #define		VERSION_TYPE		"Alpha "	// RC, Alpha, Beta, etc.
 
 #define		VERSION_VARIANT		"Pingo3D BRG"
