66cfc5655730c30e4ea2f869a75ea7038b7f4444
bgates747 <brandon.gates@gmail.com>
2024-08-27 11:08:16 -0400

    manual merge to vdp 2.10.0

--- video/version.h
+++ video/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define		VERSION_MAJOR		2
-#define		VERSION_MINOR		9
-#define		VERSION_PATCH		2
+#define		VERSION_MINOR		10
+#define		VERSION_PATCH	    0   
 #define		VERSION_CANDIDATE	5			// Optional
 #define		VERSION_TYPE		"Alpha "	// RC, Alpha, Beta, etc.
 
