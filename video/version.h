65a0fefdc0f833eaf094ea5e7043adea9f7b254c
bgates747 <brandon.gates@gmail.com>
2024-08-27 11:18:07 -0400

    bump pingo version to alpha 6

--- video/version.h
+++ video/version.h
@@ -4,7 +4,7 @@
 #define		VERSION_MAJOR		2
 #define		VERSION_MINOR		10
 #define		VERSION_PATCH	    0   
-#define		VERSION_CANDIDATE	5			// Optional
+#define		VERSION_CANDIDATE	6			// Optional
 #define		VERSION_TYPE		"Alpha "	// RC, Alpha, Beta, etc.
 
 #define		VERSION_VARIANT		"Pingo3D BRG"
