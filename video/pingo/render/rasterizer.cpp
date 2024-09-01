0fbe3649b3b14dadaf60ef0241f0773e6f9f1cc5
bgates747 <brandon.gates@gmail.com>
2024-09-01 11:40:10 -0400

    rasterizer stubs to be populated later

---
+++ video/pingo/render/rasterizer.cpp
@@ -0,0 +1,4 @@
+#include "rasterizer.hpp"
+namespace p3d {
+// TODO: original Pingo code contained no functions we are using, but renderer class contains many things that might fit better here.
+}
---
+++ video/pingo/render/rasterizer.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace p3d {
+// TODO: original Pingo code contained no functions we are using, but renderer class contains many things that might fit better here.
+}
